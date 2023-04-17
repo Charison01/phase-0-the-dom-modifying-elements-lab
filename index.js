030af4dfa414fa9c377a2508038dbb7332ca0611
Kelvin <kelvinkimaru@gmail.com>
2023-04-17 12:12:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+//Remove the main with ID in the DOM
+const body = document.querySelector('body')
+body.remove(main)
+
+//Add newHeader with ID victory
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+//Adding contents inside
+newHeader.innerHTML = "YOUR-NAME is the champion"
